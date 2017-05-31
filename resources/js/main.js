275e04b4e90ba8ab2c50e12f60a72e6eaf2029a1
josh hollis <pauljhollis@gmail.com>
2017-05-31 19:26:00 -0400

    Add animation on click

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,8 @@
 $( document ).ready(function() {
-    $(".bannerText").fadeIn(3000);
+  $("#bannerText").fadeIn(2000);
+  $(".showSites").click(function() {
+    $('#bannerText').animate({'opacity' : 0 , 'marginTop': "-=500px"} , 750 , function(){
+       $(this).fadeOut(0);
+    });
+   });
 });
